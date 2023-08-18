a9d19c75943e3792d1dbb95df7fa19168bf7b53d
yangyanlong <yangyanlong@supconit.com>
2023-08-18 08:48:15 +0800

    feat: add vao

--- OpenGL-Learn/src/Application.cpp
+++ OpenGL-Learn/src/Application.cpp
@@ -117,6 +117,9 @@ int main(void)
 	/* Initialize the library */
 	if (!glfwInit())
 		return -1;
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 
 
@@ -148,6 +151,11 @@ int main(void)
 		2, 3, 0
 	};
 
+	unsigned int vao;
+	GLCall(glGenVertexArrays(1, &vao));
+	GLCall(glBindVertexArray(vao));
+
+
 	unsigned int buffer;
 	GLCall(glGenBuffers(1, &buffer));
 	GLCall(glBindBuffer(GL_ARRAY_BUFFER, buffer));
@@ -157,7 +165,7 @@ int main(void)
 	GLCall(glEnableVertexAttribArray(0));
 	GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0));
 
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	
 
 	unsigned int ibo;
 	GLCall(glGenBuffers(1, &ibo));
@@ -173,6 +181,11 @@ int main(void)
 	ASSERT(location != -1);
 	glUniform4f(location, 0.2f, 0.3f, 0.8f, 1.0f);
 
+	GLCall(glBindVertexArray(0));
+	GLCall(glBindBuffer(GL_ARRAY_BUFFER, 0));
+	GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0));
+	GLCall(glUseProgram(0));
+
 	glfwSwapInterval(1);
 
 	float r = 0.0f;
@@ -182,8 +195,12 @@ int main(void)
 	{
 		/* Render here */
 		GLCall(glClear(GL_COLOR_BUFFER_BIT));
-
+		GLCall(glUseProgram(shader));
 		GLCall(glUniform4f(location, r, 0.3f, 0.8f, 1.0f));
+
+		GLCall(glBindVertexArray(vao));
+		GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo));
+		
 
 		GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
 
