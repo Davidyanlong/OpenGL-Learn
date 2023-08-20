932ad66fa46867c92ad7da2660d501f0cedc9bbb
yanlong.yang <yyl_love_you@126.com>
2023-08-20 12:02:55 +0800

    feat: add Abstraction Shader Classes

--- OpenGL-Learn/src/Renderer.h
+++ OpenGL-Learn/src/Renderer.h
@@ -3,9 +3,9 @@
 #include <GL/glew.h>
 
 #define ASSERT(x) if(!(x)) __debugbreak();
-#define GLCall(x) { GLClearError();\
+#define GLCall(x)  GLClearError();\
 	x;\
-	ASSERT(GLLogCall(#x, __FILE__, __LINE__)) }
+	ASSERT(GLLogCall(#x, __FILE__, __LINE__)) 
 
 void GLClearError();
 bool GLLogCall(const char* function, const char* file, int line);
