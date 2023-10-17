429d8a78a95a6f50c448be9f317f02ffa890a4fc
yanlong.yang <yyl_love_you@126.com>
2023-10-17 09:22:59 +0800

    feat: add glsl resource

--- OpenGL-Learn/src/game/game.cpp
+++ OpenGL-Learn/src/game/game.cpp
@@ -17,3 +17,51 @@ using namespace irrklang;
 
 
 // Game-related state data
+SpriteRenderer*		Renderer;
+GameObject*			Player;
+BallObject*			Ball;
+ParticleGenerator*	Particles;
+PostProcessor*		Effects;
+ISoundEngine*		SoundEngine = createIrrKlangDevice();
+TextRenderer*		Text;
+
+float ShakeTime = 0.0f;
+
+Game::Game(unsigned int width, unsigned int height)
+	:State(GAME_MENU), Keys(), KeysProcessed(), Width(width), Height(height), Level(0), Lives(3)
+{
+
+}
+
+Game::~Game()
+{
+	delete Renderer;
+	delete Player;
+	delete Ball;
+	delete Particles;
+	delete Effects;
+	delete Text;
+	SoundEngine->drop();
+}
+
+void Game::Init()
+{
+	// load shaders
+	ResourceManager::LoadShader("src/game/shaders/sprite.vert", "src/game/shaders/sprite.frag", nullptr, "sprite");
+	ResourceManager::LoadShader("src/game/shaders/particle.vert", "src/game/shaders/particle.frag", nullptr, "particle");
+	ResourceManager::LoadShader("src/game/shaders/post_processing.vert", "src/game/shaders/post_processing.frag", nullptr, "postprocessing");
+
+	//configure shaders
+	glm::mat4 projection = glm::ortho(0.0f, static_cast<float>(this->Width), static_cast<float>(this->Height), 0.0f, -1.0f, 1.0f);
+	ResourceManager::GetShader("sprite").Use().SetInteger("sprite", 0);
+	ResourceManager::GetShader("sprite").Use().SetMatrix("projection", projection);
+	ResourceManager::GetShader("particle").Use().SetInteger("sprite", 0);
+	ResourceManager::GetShader("particle").Use().SetMatrix("projection", projection);
+	// load textures
+
+
+
+
+}
+
+
