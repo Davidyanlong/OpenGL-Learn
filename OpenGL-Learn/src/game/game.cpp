e7c01de4a7904336cafc464b6bf661ee54126352
yanlong.yang <yyl_love_you@126.com>
2023-11-06 09:19:22 +0800

    feat: add collision code

--- OpenGL-Learn/src/game/game.cpp
+++ OpenGL-Learn/src/game/game.cpp
@@ -277,8 +277,195 @@ void Game::SpawnPowerUps(GameObject& block)
 
 }
 
+void ActivatePowerUp(PowerUp& powerUp)
+{
+	if (powerUp.Type == "speed")
+	{
+		Ball->Velocity *= 1.2;
+	}
+	else if (powerUp.Type == "sticky")
+	{
+		Ball->Sticky = true;
+		Player->Color = glm::vec3(1.0f, 0.5f, 1.0f);
+	}
+	else if (powerUp.Type == "pass-through")
+	{
+		Ball->PassThrough = true;
+		Ball->Color = glm::vec3(1.0f, 0.5f, 0.5f);
+	}
+	else if (powerUp.Type == "pad-size-increase")
+	{
+		Player->Size.x += 50;
+	}
+	else if (powerUp.Type == "confuse")
+	{
+		if (!Effects->Chaos)
+			Effects->Confuse = true; // only activate if chaos wasn't already active
+	}
+	else if (powerUp.Type == "chaos")
+	{
+		if (!Effects->Confuse)
+			Effects->Chaos = true;
+	}
+}
+
+
+// collision detection
+bool CheckCollision(GameObject&, GameObject&);
+Collision CheckCollision(BallObject&, GameObject&);
+Direction VectorDirection(glm::vec2 closest);
+
 
 void Game::DoCollisions()
 {
+	for (GameObject& box : this->Levels[this->Level].Bricks)
+	{
+		if (!box.Destroyed)
+		{
+			Collision collision = CheckCollision(*Ball, box);
+			if (std::get<0>(collision)) // if collision is true
+			{
+				// destroy block if not solid
+				if (!box.IsSolid)
+				{
+					box.Destroyed = true;
+					this->SpawnPowerUps(box);
+					SoundEngine->play2D("src/game/resources/audio/bleep.mp3", false);
+				}
+				else
+				{
+					// if block is solid, enable shake effect
+					ShakeTime = 0.05f;
+					Effects->Shake = true;
+					SoundEngine->play2D("src/game/resources/audio/bleep.map3", false);
+				}
+				// collision reslution
+				Direction dir = std::get<1>(collision);
+				glm::vec2 diff_vector = std::get<2>(collision);
+				if (!(Ball->PassThrough && !box.IsSolid)) // don't do collision resoution on non-solid bricks if pass-through is activated
+				{
+					if (dir == LEFT || dir == RIGHT) // horizontal collision
+					{
+						Ball->Velocity.x = -Ball->Velocity.x;  // reverse hrizontal velocity
+						// relocate
+						float penetration = Ball->Radius - std::abs(diff_vector.x);
+						if (dir == LEFT)
+							Ball->Position.x += penetration;
+						else
+							Ball->Position.x -= penetration;
+					}
+					else // vectical collision
+					{
+						Ball->Velocity.y = -Ball->Velocity.y; // reverse vertical velocity
+						// relocae
+						float penetration = Ball->Radius - std::abs(diff_vector.y);
+						if (dir == UP)
+							Ball->Position.y -= penetration;  // move ball back up
+						else
+							Ball->Position.y += penetration;  // move ball back down
+					}
+				}
+			}
+		}
+	}
+
+	// also check collisions on PowerUps an if so, activate them
+	for (PowerUp& powerUp : this->PowerUps)
+	{
+		if (!powerUp.Destroyed)
+		{
+			// first check if powerup passed bottom edge, if so: keep as inactive and destroy
+			if (powerUp.Position.y >= this->Height)
+				powerUp.Destroyed = true;
+
+			if (CheckCollision(*Player, powerUp))
+			{
+				// collided with player, now activate powerup
+				ActivatePowerUp(powerUp);
+				powerUp.Destroyed = true;
+				powerUp.Activated = true;
+				SoundEngine->play2D("src/game/resources/audio/powerup.wav", false);
+			}
+		}
+	}
+
+	// and finally check collisions for player pad (unless stuck)
+	Collision result = CheckCollision(*Ball, *Player);
+	if (!Ball->Stuck && std::get<0>(result))
+	{
+		// check where it hit the board, and change velocity based on where it hit the board
+		float centerBoard = Player->Position.x + Player->Size.x / 2.0f;
+		float distance = (Ball->Position.x + Ball->Radius) - centerBoard;
+		float percentage = distance / (Player->Size.x / 2.0f);
+		// then move accordingly
+		float strength = 2.0f;
+		glm::vec2 oldVelocity = Ball->Velocity;
+		Ball->Velocity.x = INITIAL_BALL_VELOCITY.x * percentage * strength;
+		//Ball->Velocity.y = -Ball->Velocity.y;
+		Ball->Velocity = glm::normalize(Ball->Velocity) * glm::length(oldVelocity); // keep speed consistent over both axes (multiply by length of old velocity, so total strength is not changed)
+		// fix sticky paddle
+		Ball->Velocity.y = -1.0f * abs(Ball->Velocity.y);
+
+		// if Sticky powerup is activated, also stick ball to paddle once new velocity vectors were calculated
+		Ball->Stuck = Ball->Sticky;
+
+		SoundEngine->play2D("src/game/resources/audio/bleep.wav", false);
+	}
 }
 
+
+bool CheckCollision(GameObject& one, GameObject& two) // AABB - AABB collision
+{
+	// collision x-axis?
+	bool collisionX = one.Position.x + one.Size.x >= two.Position.x &&
+		two.Position.x + two.Size.x >= one.Position.x;
+	// collision y-axis?
+	bool collisionY = one.Position.y + one.Size.y >= two.Position.y &&
+		two.Position.y + two.Size.y >= one.Position.y;
+	// collision only if on both axes
+	return collisionX && collisionY;
+}
+
+Collision CheckCollision(BallObject& one, GameObject& two) // AABB - Circle collision
+{
+	// get center point circle first 
+	glm::vec2 center(one.Position + one.Radius);
+	// calculate AABB info (center, half-extents)
+	glm::vec2 aabb_half_extents(two.Size.x / 2.0f, two.Size.y / 2.0f);
+	glm::vec2 aabb_center(two.Position.x + aabb_half_extents.x, two.Position.y + aabb_half_extents.y);
+	// get difference vector between both centers
+	glm::vec2 difference = center - aabb_center;
+	glm::vec2 clamped = glm::clamp(difference, -aabb_half_extents, aabb_half_extents);
+	// now that we know the clamped values, add this to AABB_center and we get the value of box closest to circle
+	glm::vec2 closest = aabb_center + clamped;
+	// now retrieve vector between center circle and closest point AABB and check if length < radius
+	difference = closest - center;
+
+	if (glm::length(difference) < one.Radius) // not <= since in that case a collision also occurs when object one exactly touches object two, which they are at the end of each collision resolution stage.
+		return std::make_tuple(true, VectorDirection(difference), difference);
+	else
+		return std::make_tuple(false, UP, glm::vec2(0.0f, 0.0f));
+}
+
+// calculates which direction a vector is facing (N,E,S or W)
+Direction VectorDirection(glm::vec2 target)
+{
+	glm::vec2 compass[] = {
+		glm::vec2(0.0f, 1.0f),	// up
+		glm::vec2(1.0f, 0.0f),	// right
+		glm::vec2(0.0f, -1.0f),	// down
+		glm::vec2(-1.0f, 0.0f)	// left
+	};
+	float max = 0.0f;
+	unsigned int best_match = -1;
+	for (unsigned int i = 0; i < 4; i++)
+	{
+		float dot_product = glm::dot(glm::normalize(target), compass[i]);
+		if (dot_product > max)
+		{
+			max = dot_product;
+			best_match = i;
+		}
+	}
+	return (Direction)best_match;
+}
